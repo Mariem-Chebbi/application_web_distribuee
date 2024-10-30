1abd3e2af024b5a560549ae1ce94dff7c95157f8
mariemchebbi <mariem.chebbi@esprit.tn>
2024-10-30 23:10:47 +0100

    first commit

---
+++ pet_care_alimentation/src/main/java/com/example/pet_care_alimentation/repositories/PlanAlimentaireRepository.java
@@ -0,0 +1,9 @@
+package com.example.pet_care_alimentation.repositories;
+
+import com.example.pet_care_alimentation.entities.PlanAlimentaire;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlanAlimentaireRepository extends JpaRepository<PlanAlimentaire,Long> {
+}
