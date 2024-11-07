e29cb8ca0c1b7ae4907ba2d1f76849cc4b70781e
oumeima <oumeima.temani@esprit.tn>
2024-11-07 20:52:39 +0100

    Pet_Care_Appointment

--- Pet_Care_Appointment/src/main/java/tn/esprit/Pet_Care_Appointment/PetCareAppointmentApplication.java
+++ Pet_Care_Appointment/src/main/java/tn/esprit/Pet_Care_Appointment/PetCareAppointmentApplication.java
@@ -2,7 +2,9 @@ package tn.esprit.Pet_Care_Appointment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "tn.esprit.Pet_Care_Appointment.Service")
 @SpringBootApplication
 public class PetCareAppointmentApplication {
 
--- Pet_Care_Appointment/src/test/java/tn/esprit/Pet_Care_Appointment/PetCareAppointmentApplicationTests.java
+++ Pet_Care_Appointment/src/test/java/tn/esprit/Pet_Care_Appointment/PetCareAppointmentApplicationTests.java
@@ -1,9 +1,16 @@
 package tn.esprit.Pet_Care_Appointment;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import tn.esprit.Pet_Care_Appointment.Service.IService.IAppointmentService;
+
+
+@RunWith(SpringRunner.class)
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class PetCareAppointmentApplicationTests {
@@ -12,4 +19,7 @@ class PetCareAppointmentApplicationTests {
 	void contextLoads() {
 	}
 
+	@MockBean
+    private IAppointmentService appointmentService;
 }
+
