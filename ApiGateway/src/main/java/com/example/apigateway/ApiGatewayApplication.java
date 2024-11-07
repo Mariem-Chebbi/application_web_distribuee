54ba6e655529fbac53e826fcdf14c7dd07b292d4
dhafer99 <dhafer.souid@esprit.tn>
2024-11-07 14:09:40 +0100

    addedgatewayintegrationwithkeycloack

--- ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+@PropertySource("classpath:gateway.properties")
+@SpringBootApplication(scanBasePackages = "org.springframework.security.oauth2.jwt")
 
-@SpringBootApplication
+
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
--- pet_care_alimentation/src/main/java/com/example/pet_care_alimentation/repositories/PlanAlimentaireRepository.java
+++ pet_care_alimentation/src/main/java/com/example/pet_care_alimentation/repositories/PlanAlimentaireRepository.java
@@ -1,6 +1,7 @@
 package com.example.pet_care_alimentation.repositories;
 
 import com.example.pet_care_alimentation.entities.PlanAlimentaire;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
