7f559af61466983b41b88017d9fe6bd410e8fafb
fareschakroun <fares.chakroun@esprit.tn>
2024-11-07 23:39:57 +0100

    configserve

--- Pet_Care_veterinaire/src/main/java/com/example/pet_care_veterinaire/PetCareVeterinaireApplication.java
+++ Pet_Care_veterinaire/src/main/java/com/example/pet_care_veterinaire/PetCareVeterinaireApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetCareServiceApplication {
+public class PetCareVeterinaireApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetCareServiceApplication.class, args);
+        SpringApplication.run(PetCareVeterinaireApplication.class, args);
     }
 
 }
